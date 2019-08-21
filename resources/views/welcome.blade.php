695bd0773a35ff1e032ee1fd28746b51eb4e1fd8
Matt Stauffer <matt@tighten.co>
2019-08-21 10:16:59 -0400

    Re-work for WhyPHP

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,44 +32,15 @@
         <div class="flex flex-col justify-around h-full">
             <div>
                 <h1 class="text-gray-900 text-center font-bold text-5xl mb-6">
-                    OMG 😲 PHP
+                    Why PHP?
                 </h1>
                 <div class="mb-8 leading-normal">
-                    <p><strong>What's this?</strong><br>A placeholder for one site--or maybe two separate sites--which, once well-planned, will have real designs and marketing and stuff. By Matt Stauffer of Tighten, with the goals to:<br><br>
-                    A) welcome newcomers into PHP in a healthy and friendly and easy-to-learn and safe way, and<br>
-                    B) combat the misperception that PHP is dead and shouldn't be learned or used for modern projects.</p>
+                    <p><strong>What's this?</strong><br>A placeholder for a site designed to answer the question "Why PHP?". By Matt Stauffer of Tighten, with the goals to:<br><br>
+                    A) combat the misperception that PHP is dead and shouldn't be learned or used for modern projects.<br>
+                    B) encourage new developers and bootcamps and companies to consider PHP<br>
+                </p>
                 </div>
                 <ul>
-                    <li class="mb-4">
-                        <div class="font-bold mb-2 text-lg">Learn PHP for free</div>
-
-                        <ul class="ml-4 leading-normal list-disc list-inside">
-                            <li><a href="https://laracasts.com/series/php-for-beginners" class="no-underline hover:underline font-normal text-teal-900">Laracasts Video Series "The PHP Practitioner"</a></li>
-                            <li><a href="https://daylerees.com/php-pandas/" class="no-underline hover:underline font-normal text-teal-900">PHP Pandas Online Book</a></li>
-                        </ul>
-                    </li>
-                    <li class="mb-4">
-                        <div class="font-bold mb-2 text-lg">Ask questions about PHP</div>
-
-                        <ul class="ml-4 leading-normal list-disc list-inside">
-                            <li>?</li>
-                        </ul>
-                    </li>
-                    <li class="mb-4">
-                        <div class="font-bold mb-2 text-lg">Welcoming PHP organizations</div>
-
-                        <ul class="ml-4 leading-normal list-disc list-inside">
-                            <li><a href="https://osmihelp.org/" class="no-underline hover:underline font-normal text-teal-900">OSMI</a></li>
-                            <li>?</li>
-                        </ul>
-                    </li>
-                    <li class="mb-4">
-                        <div class="font-bold mb-2 text-lg">The Real World</div>
-
-                        <ul class="ml-4 leading-normal list-disc list-inside">
-                            <li><a href="https://meetup.com/topics/php" class="no-underline hover:underline font-normal text-teal-900">Find a meet-up</a></li>
-                        </ul>
-                    </li>
                     <li class="mb-4">
                         <div class="font-bold mb-2 text-lg">Isn't PHP dead?</div>
 
@@ -80,9 +51,17 @@
                             <li><a href="https://stitcher.io/blog/php-in-2019" class="no-underline hover:underline font-normal text-teal-900">This tweet and its replies: <a href="https://twitter.com/stauffermatt/status/1163794261906771968" class="text-sm">https://twitter.com/stauffermatt/status/1163794261906771968</a></li>
                         </ul>
                     </li>
+                   <li class="mb-4">
+                        <div class="font-bold mb-2 text-lg">Other stuff I guess?</div>
+
+                        <ul class="ml-4 leading-normal list-disc list-inside">
+                            <li><a href="/pdf/enterprise-php-ireland-whitepaper.pdf" class="no-underline hover:underline font-normal text-teal-900">10-year old but still interesting white paper on PHP viability in the enterprise</a></li>
+                        </ul>
+                    </li>
                 </ul>
+                <p class="mt-12">@todo: Lots more content gathering and planning, then a design and generating materials and then lots of marketing etc.</p>
             </div>
-            <div><a href="https://github.com/tightenco/omgphp">Source on GitHub</a></div>
+            <div><a href="https://github.com/tightenco/whyphp" class="text-teal-900 hover:underline">Source on GitHub</a></div>
         </div>
 
     </div>
